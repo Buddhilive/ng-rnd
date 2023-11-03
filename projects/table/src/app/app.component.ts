306016100707748cfd74669358a5b04d656cdc79
Buddhi Kavindra Ranasinghe <visitbudkavin@gmail.com>
2023-11-03 08:51:23 +0530

    dynamic table

--- projects/table/src/app/app.component.ts
+++ projects/table/src/app/app.component.ts
@@ -1,39 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
+  @ViewChild("fbDynamicGrid") fbDynamicGrid: ElementRef<HTMLTableElement>;
+
   numCells = 3;
-  numRows = 3;
+  numRows = 5;
   gridData: Array<any>;
+
+  currentRowIndex: number;
+  currentCellIndex: number;
+  cellInput: HTMLInputElement;
+
+  isRowEnd: boolean = true;
+  isColEnd: boolean = true;
 
   ngOnInit() {
     this.gridData = this.createDataGrid(this.numRows);
     console.log(this.gridData);
   }
 
-  getRow(evt: MouseEvent) {
-    evt.stopPropagation();
-    const cell = evt.target as HTMLInputElement;
-    console.log(this.gridData);
-    if(cell.checked) {
-      this.gridData[cell.dataset.row][cell.dataset.cell] = 1;
+  getRow(evt: MouseEvent, row: number, cell: number) {
+    this.currentCellIndex = cell;
+    this.currentRowIndex = row;
+    this.cellInput = evt.target as HTMLInputElement;
+    const tableRows = this.fbDynamicGrid.nativeElement.rows;
+    const cellDiff = tableRows[this.currentRowIndex].cells.length - this.currentCellIndex;
+    const rowDiff = tableRows.length - this.currentRowIndex;
+
+    if(parseInt(this.cellInput.value) > 0 && this.cellInput.value.length > 0 && cellDiff > 0) {
+      if(parseInt(this.cellInput.value) <= rowDiff) {
+        this.isRowEnd = false;
+      } else {
+        this.isRowEnd = true;
+      }
+  
+      if(parseInt(this.cellInput.value) <= cellDiff) {
+        this.isColEnd = false;
+      } else {
+        this.isColEnd = true;
+      }
     } else {
-      this.gridData[cell.dataset.row][cell.dataset.cell] = 0;
+      this.isRowEnd = true;
+      this.isColEnd = true;
     }
-    console.log(this.gridData);
+
+    console.log(this.currentCellIndex, cellDiff);
   }
 
-  getSelectedCell(evt: MouseEvent) {
-    // console.log(evt, this.gridData);
+  getCellDetails(evt: MouseEvent) {
+    const cell = evt.target as HTMLTableCellElement;
+    console.log(evt);
+  }
+
+  mergeCells(isRow: boolean) {
+    const selectedCell: HTMLTableCellElement = this.fbDynamicGrid.nativeElement.rows[this.currentRowIndex].cells[this.currentCellIndex];
+    if(isRow) {
+      selectedCell.setAttribute('rowspan', this.cellInput.value);
+      for (let i = this.currentRowIndex; i < (parseInt(this.cellInput.value) + this.currentRowIndex); i++) {
+        if(i !== this.currentRowIndex) {
+          this.fbDynamicGrid.nativeElement.rows[i].cells[this.currentCellIndex].remove();
+        }
+      }
+    } else {
+      selectedCell.setAttribute('colspan', this.cellInput.value);
+      for (let i = this.currentCellIndex; i < (parseInt(this.cellInput.value) + this.currentCellIndex); i++) {
+        if(i !== this.currentCellIndex) {
+          this.fbDynamicGrid.nativeElement.rows[this.currentRowIndex].cells[i].remove();
+        }
+      }
+    }
+    console.log(selectedCell);
   }
 
   private createDataGrid(rows: number) {
     const newArray = [];
-    for(let i = 0; i < rows; i++) {
+    for (let i = 0; i < rows; i++) {
       newArray.push(this.createCells(this.numCells));
     }
     return newArray;
@@ -41,10 +87,9 @@ export class AppComponent {
 
   private createCells(cells: number) {
     const newArray = [];
-    for(let i = 0; i < cells; i++) {
+    for (let i = 0; i < cells; i++) {
       newArray.push(0);
     }
     return newArray;
   }
-
 }
