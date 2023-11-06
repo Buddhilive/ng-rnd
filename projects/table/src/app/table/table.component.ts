e4a3bf296aa22a4da675a1b6fd66bf32f9d95657
Buddhi Kavindra Ranasinghe <visitbudkavin@gmail.com>
2023-11-06 13:13:34 +0530

    add basic colspan

--- projects/table/src/app/table/table.component.ts
+++ projects/table/src/app/table/table.component.ts
@@ -26,18 +26,73 @@ export class TableComponent implements OnInit {
 
     if(tableRef.classList.contains('selected')) {
       const cellInfo = {
-        row: tableRef.ariaRowIndex,
+        row: tableRef.getAttribute('aria-rowindex'),
         cell: tableRef.cellIndex
       };    
       this.selectedCells.push(cellInfo);
     } else {
       const uniqueArray = this.selectedCells;
-      this.selectedCells = uniqueArray.filter((obj: any) => obj.row != tableRef.ariaRowIndex || obj.cell != tableRef.cellIndex);
-      //console.log(uniqueArray)
+      this.selectedCells = uniqueArray.filter((obj: any) => obj.row != tableRef.getAttribute('aria-rowindex') || obj.cell != tableRef.cellIndex);
     }
 
+    this.selectedCells.sort((a, b) => {
+      if (a.row === b.row) {
+        return a.cell - b.cell;
+      }
+      return a.row - b.row;
+    });
 
     console.info(this.selectedCells);
+  }
+
+  mergeCells() {
+    const colSpanArray: Array<any> = [];
+    const rowSpanArray: Array<any> = [];
+    
+    this.selectedCells.map((cell: any) => {
+      if(!rowSpanArray.includes(cell.row)) {
+        rowSpanArray.push(cell.row);
+      }
+    });
+
+    for (let i in rowSpanArray) {
+      const cols = [];
+      for (let cell in this.selectedCells) {
+        if(this.selectedCells[cell].row == rowSpanArray[i]) {
+          cols.push(this.selectedCells[cell].cell);
+        }
+      }
+      colSpanArray.push({
+        [rowSpanArray[i]]: cols
+      });
+    }
+
+    for (let row in colSpanArray) {
+      const rowIndex = rowSpanArray[row];
+      const colSpan = colSpanArray[row][rowIndex];
+      this.fbDynamicGrid.nativeElement.rows[rowIndex].cells[colSpan[0]].colSpan = colSpan.length;
+      for(let i = 1; i < colSpan.length; i++) {
+        console.log(colSpan[i], this.fbDynamicGrid.nativeElement.rows[rowIndex].cells[colSpan[0] + 1]);
+        this.fbDynamicGrid.nativeElement.rows[rowIndex].cells[colSpan[0] + 1].remove();
+      }
+      console.log(colSpan[colSpan.length - 1]);
+    }
+    this.selectedCells = [];
+    console.log(this.selectedCells, rowSpanArray, colSpanArray);
+
+  }
+
+  rowUpdated(evt: Event) {
+    console.log(evt);
+  }
+
+  private isInOrder(numArray: Array<number>) {
+    for(let i = numArray[0]; i < (numArray.length + numArray[0]); i++) {
+      if(numArray[i] !== (i + numArray[0])) {
+        return false;
+      }
+    }
+    return true;
   }
 
   private createDataGrid(rows: number) {
