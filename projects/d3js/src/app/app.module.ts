c07145116c893993741d583a9e9ac747638811b2
Buddhi Kavindra Ranasinghe <visitbudkavin@gmail.com>
2024-02-29 16:00:00 +0530

    add indexeddb

--- projects/d3js/src/app/app.module.ts
+++ projects/d3js/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CompAModule, CompBModule } from 'projects/multi-entry/public_api';
+import { IndexdbComponent } from './indexdb/indexdb.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
 /* import { CompAModule } from 'multi-entry/comp-a';
 import { CompBModule } from 'multi-entry/comp-b'; */
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexdbComponent
   ],
   imports: [
     BrowserModule,
     CompAModule,
-    CompBModule
+    CompBModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/d3js/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router";
+import { IndexdbComponent } from "./indexdb/indexdb.component";
+
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: IndexdbComponent
+    }
+];
